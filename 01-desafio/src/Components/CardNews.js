8e07fd00991cf1817ef8dccf0cac389e3ec4197e
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-29 06:40:30 -0300

    Continuação do desafio

---
+++ 01-desafio/src/Components/CardNews.js
@@ -0,0 +1,53 @@
+class Cardnews extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+        
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left;");
+
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/foto-default.jpg";
+        newsImage.alt = "Foto da noticia."
+        newsImage.width = this.getAttribute("width");
+        newsImage.height = this.getAttribute("height");
+        cardRight.appendChild(newsImage);
+
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles(){
+
+    }
+}
+
+customElements.define("card-news", Cardnews);
