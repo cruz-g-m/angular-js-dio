89a8f953b70d1d09241d1370d01cbf1f1a19c93e
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-30 03:47:52 -0300

    Conclusão do desafio 1

--- 01-desafio/src/Components/CardNews.js
+++ 01-desafio/src/Components/CardNews.js
@@ -3,8 +3,8 @@ class Cardnews extends HTMLElement{
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.appendChild(this.build())
-        shadow.appendChild(this.styles())
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
 
     build(){
@@ -12,7 +12,7 @@ class Cardnews extends HTMLElement{
         componentRoot.setAttribute("class", "card");
         
         const cardLeft = document.createElement("div");
-        cardLeft.setAttribute("class", "card__left;");
+        cardLeft.setAttribute("class", "card__left");
 
         const autor = document.createElement("span");
         autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
@@ -46,7 +46,43 @@ class Cardnews extends HTMLElement{
     }
 
     styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+        .card{
+            width: 80%;
+            box-shadow: 9px 7px 24px -10px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 9px 7px 24px -10px rgba(0,0,0,0.75);
+            -moz-box-shadow: 9px 7px 24px -10px rgba(0,0,0,0.75);
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        
+        .card__left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        .card__left > span{
+            font-weight: 400;
+        }
+        
+        .card__left > a{
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        
+        .card__left > p {
+            color: gray;
+        }
 
+        `;
+
+        return style;
     }
 }
 
